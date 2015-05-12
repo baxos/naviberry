51b49aa9b00511275f76203654435bff458b97ca
baxos <mrbacher@gmail.com>
2015-05-12 23:12:09 +0900

    Changeing code to C++11 instead of C, to provide more abstraction(through classes)

--- src/srv/cpp-fork/motordriver.h
+++ src/srv/cpp-fork/motordriver.h
@@ -1,25 +1,19 @@
-#include <stdint.h>
+#include <cstdint>
 
-struct MOTOR
-{
-	uint8_t pin_r;  // Pin 2A
-	uint8_t pin_l;  // Pin 1A
-	uint8_t pin_e; 	// Enable pin
-	uint8_t direction;
-	uint8_t running;
-};
 
 class DC_Motor 
 {
-  uint8_t pin_r;// pin 2a
-  uint8_t pin_l;// pin 1a
-  uint8_t pin_e;// pin enable
-  uint_8t direction;// 0 for right 1 for left
+  uint8_t pin_r;             // pin 2a
+  uint8_t pin_l;             // pin 1a
+  uint8_t pin_e;             // pin enable
+  uint8_t direction;         // 0 for right 1 for left
 
-}
+ public:
+  DC_Motor(uint8_t _pin_e, uint8_t _pin_l, uint8_t _pin_r);
+  void Motor_Start();
+  void Motor_Stop();
+  void Motor_setDirection(uint8_t _direct);
+  void Motor_getDirection(uint8_t _direct);
 
-int Motor_Init(struct MOTOR** motor, uint8_t pin_e, uint8_t pin_1a, uint8_t pin_2a);
-void Motor_Start(struct MOTOR** motor);
-void Motor_Stop(struct MOTOR** motor);
-void Motor_setDirection(struct MOTOR** motor, uint8_t dir);
+};
 
