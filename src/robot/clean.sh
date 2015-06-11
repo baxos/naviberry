ba161464987f39f4ad73afd9b016d15a882b2faf
baxos <mrbacher@gmail.com>
2015-06-11 15:02:06 +0000

    Removed old code

--- src/robot/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Shell script for cleaning up after compilation
-
-echo "Removing netpacket.o"
-rm netpacket.o
-echo "Removing network.o"
-rm network.o
-echo "Removing llist.o"
-rm llist.o
