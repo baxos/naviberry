c906f070b444a514d1cc3a2d8376005e0838131a
baxos <mrbacher@gmail.com>
2015-06-13 02:29:10 +0200

    Added scheduler class

---
+++ src/server/include/scheduler.cpp
@@ -0,0 +1,87 @@
+#include <chrono>
+#include "./scheduler.hpp"
+
+
+
+
+
+
+// =================================== Scheduler Class ================================== //
+
+
+// Scheduler Constructor function
+// Initialize class variables
+Scheduler::Scheduler()
+{
+  // Set counters
+  hardwareCounter = hardwareCounterMax;
+  sensorCounter   = sensorCounterMax;
+  networkCounter  = networkCounterMax;
+
+  // Set flags
+  hardwareFlag = false;
+  sensorFlag   = false;
+  networkFlag  = false;
+  firstRun     = true;
+
+  // For time keeping
+  total_msec = 0;
+  total_sec  = 0;
+  count_msec = 0;
+}
+
+// Scheduler Update void function
+// Update time, counters and flags
+void Scheduler::Update()
+{
+  long ms_now = getmsecNow();
+
+  if (firstRun)
+    {
+      ms_last_run = ms_now;
+      firstRun = false;
+    }
+
+  if ( (ms_now - ms_last_run) != 0)
+    {
+      // We have a minimum tick of 1 
+      auto deltaT = ms_now - ms_last_run;
+
+      // Vaildate it's correct measurement(?)
+      if (deltaT > 0)
+	{
+	  
+	  // Decrease counters
+	  hardwareCounter -= deltaT;
+	  sensorCounter   -= deltaT;
+	  networkCounter  -= deltaT;
+
+	  // Set flags if needed
+	  if (hardwareCounter <= 0)
+	    {
+	      hardwareFlag = true;
+	    }
+	  if (sensorCounter <= 0)
+	    {
+	      sensorFlag = true;
+	    }
+	  if (networkCounter <= 0)
+	    {
+	      networkFlag = true;
+	    }
+	}
+    }
+  else
+    {
+      // Do nothing
+    }
+}
+
+// Scheduler getmsecNow long function
+// Returns msec since epoch
+long Scheduler::getmsecNow()
+{
+  std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds> ( std::chrono::high_resolution_clock::now().time_since_epoch());
+
+  return (long) ms.count();
+}
---
+++ src/server/include/scheduler.hpp
@@ -0,0 +1,53 @@
+#ifndef SCHEDULER__HPP
+#define SCHEDULER__HPP
+
+#include <iostream>
+#include <cstdint>
+
+class Scheduler
+{
+private:
+  // For time keeping
+  bool firstRun;
+  long ms_last_run;
+  long getmsecNow();
+  // Time remembering
+  long total_msec;
+  long total_sec;
+  // Time counting
+  int16_t count_msec;
+
+  // For scheduling
+  // Counters 
+  const int16_t hardwareCounterMax = 100;
+  const int16_t sensorCounterMax = 200;
+  const int16_t networkCounterMax = 300;
+  int16_t hardwareCounter;
+  int16_t sensorCounter;
+  int16_t networkCounter;
+  // Flags
+  bool hardwareFlag;
+  bool sensorFlag;
+  bool networkFlag;
+
+public:
+  // Constructor and Destructor
+  Scheduler();
+  ~Scheduler();
+
+  // Get flag status
+  bool getHardwareFlag() { return hardwareFlag; };
+  bool getSensorFlag() { return sensorFlag; };
+  bool getNetworkFlag() { return networkFlag; };
+
+  // Reset flag to false and reset counters to counter max
+  void resetHardwareFlag() { hardwareCounter=hardwareCounterMax; hardwareFlag = false; };
+  void resetSensorFlag() { sensorCounter=sensorCounterMax; sensorFlag = false; };
+  void resetNetworkFlag() { networkCounter=networkCounterMax; networkFlag = false; };
+
+  // Update reference and update scheduler
+  void Update();
+};
+
+
+#endif
