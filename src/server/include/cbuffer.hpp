4a080b7ace6f108e8c47f3c9513e93eaf999a5f0
baxos <mrbacher@gmail.com>
2015-10-12 19:20:24 +0200

    3

--- src/server/include/cbuffer.hpp
+++ src/server/include/cbuffer.hpp
@@ -6,26 +6,31 @@
 #define CBUFFER__HPP
 
 #include<cstdint>
+#include<map>
 #include "./naviberryio.hpp"
 
 
-class CircularBuffer
+class NaviBuffer
 {
 private:
   uint8_t* data;
-  uint32_t buff_size;
+  uint32_t buffer_size;
   uint32_t start_pt;
   uint32_t end_pt;
+  uint16_t block_size;
+  uint16_t blocks;
+
+  std::map <uint16_t, bool> blockmap;
   uint32_t freeSpace();
 public:
-  CircularBuffer(uint32_t buffer_size);
-  ~CircularBuffer();
+  NaviBuffer(uint32_t _buffer_size);
+  ~NaviBuffer();
   void Add(uint8_t* val, uint32_t size);
   void Remove(int size);
   // gets
   uint32_t getStartPt() { return start_pt; };
   uint32_t getEndPt()  { return end_pt;   };
-  uint32_t getBufferSize() { return buff_size; };
+  uint32_t getBufferSize() { return buffer_size; };
   uint8_t* getData() { return data; };
   // sets
 };
