eb2d5459653ac296ecc30038f61c72140a92767a
baxos <mrbacher@gmail.com>
2016-12-08 17:39:11 +0000

    Added utility.hpp for fix shared_ptr issue on the pi

---
+++ src/server/include/utility.hpp
@@ -0,0 +1,13 @@
+#ifndef UTILITY__HPP
+#define UTILITY__HPP
+
+#include <memory>
+#include <utility>
+
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+  return std::unique_ptr<T> (new T(std::forward<Args>(args)...));
+}
+
+#endif 
